ce39692ca43b2a7c8d9f01b4709f218cceeec8b1
Adam Podoxin <adam.podoxin@gmail.com>
2020-06-11 11:26:34 -0700

    Created system to swtich/load pages dynamically

---
+++ js/index.js
@@ -0,0 +1,44 @@
+const pageNames = [
+    "Introduction",
+    "Occupez-vous",
+    "Faire un horaire",
+    "Couchez-vous au même temps chaque jour",
+    "Mangez les alimentations différents et saines",
+    "Allez au dehors et faire de l’exercice"
+];
+
+let pageIndex = 0;
+
+function onLoad() {
+    loadPage(pageIndex);
+}
+
+function loadPage(index) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', `pages/${index}.html`, true);
+    xhr.onreadystatechange = function () {
+        if (this.readyState !== 4)
+            return;
+
+        document.getElementById('content').innerHTML = this.responseText;
+    };
+
+    xhr.send();
+}
+
+function nextPage() {
+    if (pageIndex < pageNames.length - 1) {
+        pageIndex++;
+        loadPage(pageIndex);
+    }
+}
+
+function prevPage() {
+    if (pageIndex > 0) {
+        pageIndex--;
+        loadPage(pageIndex);
+    }
+}
+
+document.onLoad = onLoad();
